5a4604e489bd36bfcd85f88d93f0e3fd7baa1593
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-01-15 19:12:14 +0700

    Adding building the signup card

--- SwiftUI Advance/ContentView.swift
+++ SwiftUI Advance/ContentView.swift
@@ -8,9 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    @State private var email:String = ""
+    @State private var password:String = ""
+    
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Image("background-3")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                VStack(alignment: .leading, spacing: 16.0) {
+                    Text("Sign Up")
+                        .font(Font.largeTitle.bold())
+                        .foregroundColor(.white)
+                    Text("Access My World!")
+                        .font(.subheadline)
+                        .foregroundColor(Color.white.opacity(0.7))
+                    
+                    HStack {
+                        Spacer()
+                        Image(systemName: "envelope.open.fill")
+                            .foregroundColor(.white)
+                        
+                        TextField("Email", text: $email)
+                            .colorScheme(.dark)
+                            .foregroundColor(Color.white.opacity(0.7))
+                            .autocapitalization(.none)
+                            .textContentType(.emailAddress)
+                    }
+                    .frame(height: 52)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.white, lineWidth: 1.0)
+                        .blendMode(.overlay)
+                    )
+                    .background(Color("secondaryBackground").opacity(0.5))
+                    .cornerRadius(16.0)
+                    .opacity(0.8)
+                    
+                    HStack {
+                        Spacer()
+                        Image(systemName: "key.fill")
+                            .foregroundColor(.white)
+                        TextField("Password", text: $email)
+                            .colorScheme(.dark)
+                            .foregroundColor(Color.white.opacity(0.7))
+                            .autocapitalization(.none)
+                            .textContentType(.password)
+                    }
+                    .frame(height: 52)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.white, lineWidth: 1.0)
+                        .blendMode(.overlay)
+                    )
+                    .background(Color("secondaryBackground").opacity(0.5))
+                    .cornerRadius(16.0)
+                    .opacity(0.8)
+                }
+                .padding()
+            }
+            .background(
+            RoundedRectangle(cornerRadius: 30)
+                .stroke(Color.white.opacity(0.2))
+                .background(Color("secondaryBackground").opacity(0.5))
+                .background(VisualEffectBlur(blurStyle: .systemMaterialDark))
+                .shadow(color: Color("shadowColor").opacity(0.5), radius: 60, x: 0, y: 30)
+            )
+            .cornerRadius(30.0)
+            .padding(.horizontal)
+        }
     }
 
 }
