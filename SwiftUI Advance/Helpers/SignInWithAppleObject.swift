01d29425c80cf29e2eae07fae2219a222fb1d0e5
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-01-18 22:51:59 +0700

    Adding SignInWithAppleObject

--- SwiftUI Advance/Helpers/SignInWithAppleObject.swift
+++ SwiftUI Advance/Helpers/SignInWithAppleObject.swift
@@ -12,75 +12,62 @@ import FirebaseAuth
 
 class SignInWithAppleObject: NSObject {
     private var currentNonce: String?
-    
-    class SignInWithAppleObject: NSObject {
-        func signInWithApple() {
-            let request = ASAuthorizationAppleIDProvider().createRequest()
-            request.requestedScopes = [.email, .fullName]
 
-            let nonce = randomNonceString()
-            currentNonce = nonce
-            request.nonce = sha256(currentNonce!)
-        }
-        
+    public func signInWithApple() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.email, .fullName]
+
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        request.nonce = sha256(currentNonce!)
+
         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
         authorizationController.delegate = self
         authorizationController.performRequests()
-        
-        // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
-        private func randomNonceString(length: Int = 32) -> String {
-            precondition(length > 0)
-            let charset: Array<Character> =
-                Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-            var result = ""
-            var remainingLength = length
-
-            while remainingLength > 0 {
-                let randoms: [UInt8] = (0 ..< 16).map { _ in
-                    var random: UInt8 = 0
-                    let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-                    if errorCode != errSecSuccess {
-                        fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
-                    }
-                    return random
-                }
-
-                randoms.forEach { random in
-                    if remainingLength == 0 {
-                        return
-                    }
-
-                    if random < charset.count {
-                        result.append(charset[Int(random)])
-                        remainingLength -= 1
-                    }
-                }
-            }
-
-            return result
-        }
-
-        private func sha256(_ input: String) -> String {
-          let inputData = Data(input.utf8)
-          let hashedData = SHA256.hash(data: inputData)
-          let hashString = hashedData.compactMap {
-            return String(format: "%02x", $0)
-          }.joined()
-
-          return hashString
-        }
     }
-}
-
-public class SignInWithAppleObject: NSObject {
-    private var currentNonce: String?
-
-    public func signInWithApple() {...}
 
     // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
-    private func randomNonceString(length: Int = 32) -> String {...}
+    public func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
 
-    private func sha256(_ input: String) -> String {...}
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+
+        return result
+    }
+
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        return String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
 }
 
 extension SignInWithAppleObject: ASAuthorizationControllerDelegate {
@@ -100,6 +87,14 @@ extension SignInWithAppleObject: ASAuthorizationControllerDelegate {
             guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
                 print("Unable to serialize token string from data")
                 return
+            }
+            
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            Auth.auth().signIn(with: credential) { result, error in
+                guard error == nil else {
+                    print(error!.localizedDescription)
+                    return
+                }
             }
         }
     }
