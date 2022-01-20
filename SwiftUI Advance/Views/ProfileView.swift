2eea5e05247e47f5cc873b9f7e4982b053381d59
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-01-20 23:29:45 +0700

    Adding creating our profile screen

--- SwiftUI Advance/Views/ProfileView.swift
+++ SwiftUI Advance/Views/ProfileView.swift
@@ -9,7 +9,99 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile View")
+        ZStack {
+            Image("background-2")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .leading, spacing: 16) {
+                VStack {
+                    HStack(spacing: 16) {
+                        ZStack {
+                            Circle()
+                                .foregroundColor(Color("pink-gradient-1"))
+                                .frame(width: 66, height: 66, alignment: .center)
+                            Image(systemName: "person.fill")
+                                .foregroundColor(.white)
+                                .font(.system(size: 24, weight: .medium, design: .rounded))
+                        }
+                        .frame(width: 66, height: 66, alignment: .center)
+                        
+                        VStack(alignment: .leading) {
+                            Text("TheGIZzz")
+                                .foregroundColor(.white)
+                                .font(.title2)
+                                .bold()
+                            Text("View profile")
+                                .foregroundColor(.white.opacity(0.7))
+                                .font(.footnote)
+                        }
+                        
+                        Spacer()
+
+                        Button(action: {
+                            print("Segue to settings")
+                        }, label: {
+                            TextfieldIcon(iconName: "gearshape.fill", currentlyEditing: .constant(true))
+                        })
+                        
+                    }
+                    
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(.white.opacity(0.1))
+
+                    Text("Instructor at Design+Code")
+                        .foregroundColor(.white)
+                        .font(.title2.bold())
+                    
+                    Label("Awarded 10 certificates since September 2020", systemImage: "calendar")
+                        .foregroundColor(.white.opacity(0.7))
+                        .font(.footnote)
+
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(.white.opacity(0.1))
+                    
+                    HStack(spacing: 16) {
+                        Image("Twitter")
+                            .resizable()
+                            .foregroundColor(.white.opacity(0.7))
+                            .frame(width: 24, height: 24, alignment: .center)
+                        Image(systemName: "link")
+                            .foregroundColor(.white.opacity(0.7))
+                            .font(.system(size: 17, weight: .semibold, design: .rounded))
+                        Text("designcode.io")
+                            .foregroundColor(.white.opacity(0.7))
+                            .font(.footnote)
+                    }
+                }
+                .padding(16)
+
+                GradientButton(buttonTitle: "Purchase Lifetime Pro Plan") {
+                    print("IAP")
+                }
+                .padding(.horizontal, 16)
+
+                Button(action: {
+                    print("Restore")
+                }, label: {
+                    GradientText(text: "Restore Purchases")
+                        .font(.footnote.bold())
+                })
+                .padding(.bottom)
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 30)
+                    .stroke(Color.white.opacity(0.2))
+                    .background(Color("secondaryBackground").opacity(0.5))
+                    .background(VisualEffectBlur(blurStyle: .dark))
+                    .shadow(color: Color("shadowColor").opacity(0.5), radius: 60, x: 0, y: 30)
+            )
+            .cornerRadius(30)
+            .padding(.horizontal)
+        }
     }
 }
 
